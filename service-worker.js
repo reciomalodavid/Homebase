f4f121fc9cae0a89895c8f3fe50889e731ffc8ae
reciomalodavid <reciomalodavid@gmail.com>
2026-07-27 19:43:48 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE="homebase-1-4-4-shell-20260727";
-const RUNTIME_CACHE="homebase-1-4-4-runtime-20260727";
+const SHELL_CACHE="homebase-1-5-0";
+const RUNTIME_CACHE="homebase-1-5-0-runtime-20260727";
 const SHELL=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(SHELL_CACHE).then(c=>c.addAll(SHELL.map(u=>new Request(u,{cache:"reload"})))).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("homebase-")&&![SHELL_CACHE,RUNTIME_CACHE].includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
