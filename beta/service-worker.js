c56aab60cfc77894e22302e1f2a19781ad2be54d
reciomalodavid <reciomalodavid@gmail.com>
2026-08-01 19:26:42 +0200

    Añadir caché aislada para Homebase Beta

---
+++ beta/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE='homebase-beta-0.1.0';
+const SHELL=['./','./index.html','./manifest.webmanifest','./beta-icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('homebase-beta-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(!url.pathname.includes('/Homebase/beta/'))return;
+  event.respondWith(fetch(new Request(event.request,{cache:'no-store'})).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
