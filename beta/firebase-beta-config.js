48b5e94c25894752cd21e7ebf881ed1c5a546c47
reciomalodavid <reciomalodavid@gmail.com>
2026-08-11 19:08:09 +0100

    Beta: add dedicated Firebase web config

---
+++ beta/firebase-beta-config.js
@@ -0,0 +1,11 @@
+(()=>{
+  'use strict';
+  window.HOMEBASE_BETA_FIREBASE_CONFIG = Object.freeze({
+    apiKey: 'AIzaSyCxcrS5c5ErM8k4E-s4UPLXNe5WBTV_VOM',
+    authDomain: 'homebase-beta-72767.firebaseapp.com',
+    projectId: 'homebase-beta-72767',
+    storageBucket: 'homebase-beta-72767.firebasestorage.app',
+    messagingSenderId: '986505930709',
+    appId: '1:986505930709:web:06e08b083ac36a6f7abf5c'
+  });
+})();
